6e07e6be38f150bf08d87c9ae17df42943b311a3
mfclinton <mfclinton@umass.edu>
2021-05-15 22:14:36 -0400

    fixed git ignore and finished new env

--- other/Environments/Gridworld_687.py
+++ other/Environments/Gridworld_687.py
@@ -1,27 +1,42 @@
 import gym
+import matplotlib.pyplot  as plt
+from matplotlib.patches import Rectangle, Circle, Arrow
+from matplotlib.ticker import NullLocator
+import torch
 import numpy as np
 from gym import error, spaces, utils
 from gym.spaces import Discrete
 
 
 class Gridworld_687(object):
-    def __init__(self, max_steps=200, action_prob=0.8, debug=True):
+    def __init__(self, max_steps=200, action_prob=1.0, debug=True):
         self.debug = debug
         self.action_prob = action_prob
+        self.randomness = 1 - action_prob
         self.n_actions = 4
         self.n_observations = 25
         self.action_space = Discrete(self.n_actions)
         self.observation_space = Discrete(25)
+        self._n_episodes = 0
 
         self.max_steps = max_steps
+        self.step_reward = 0
         self.step_unit = 1
         self.repeat = 1
+
+        self.disp_flag = False
+
+        self.collision_reward = 0
+        self.movement_reward = 0
         
         # Gridworld Specific
-        self.width = np.sqrt(self.n_observations)
+        self.width = np.sqrt(self.n_observations).astype(np.int32)
+
+        self.static_obstacles = self.get_static_obstacles()
+
 
         if debug:
-            self.heatmap = np.zeros((width, width))
+            self.heatmap = np.zeros((self.width, self.width))
 
         self.reset()
 
@@ -31,19 +46,79 @@ class Gridworld_687(object):
     def get_embeddings(self):
         pass # TODO
 
+# TODO fix dims
     def render(self):
-        pass
+        x, y = self.curr_pos
 
+        if not self.disp_flag:
+            self.disp_flag = True
+        
+            self.currentAxis = plt.gca()
+            plt.figure(1, frameon=False) #Turns off the the boundary padding
+            # self.currentAxis.xaxis.set_major_locator(NullLocator()) #Turns of ticks of x axis
+            # self.currentAxis.yaxis.set_major_locator(NullLocator()) #Turns of ticks of y axis
+            plt.ion()                                               #To avoid display blockage
+            
+            self.circle = Circle((x / (self.width-1), 1 - y / (self.width-1)), 0.1, color='red')
+            for coords in self.static_obstacles:
+                x1, y1, _, _ = coords
+                self.currentAxis.add_patch(Rectangle((x1 / (self.width-1), 1 - y1 / (self.width-1)), 1/self.width, 1/self.width, fill=True, color='gray'))
+
+        # for key, val in self.dynamic_obs.items():
+        #     pass
+
+        for key, val in self.reward_states.items():
+            coords, cond = val
+            if cond:
+                x1, y1, _, _ = coords
+                self.objects[key] = Rectangle((x1 / (self.width-1), 1 - y1/ (self.width-1)), 1/self.width, 1/self.width, fill=True)
+                self.currentAxis.add_patch(self.objects[key])
+                
+        # Arrow
+        
+        self.objects['circle'] = Circle((x / (self.width-1), 1 - y / (self.width-1)), 0.01, color='red')
+        self.currentAxis.add_patch(self.objects['circle'])
+
+        plt.pause(1e-1)
+        for _, item in self.objects.items():
+            item.remove()
+        self.objects = {}
+
+    # reward values associated with states
     def set_rewards(self):
-        pass
+        # All rewards
+        self.G1_reward = -10 #100
+        self.G2_reward = +10 #- 5
 
     def reset(self):
         self.set_rewards()
         self.steps_taken = 0
-        pass
+        self.reward_states = self.get_reward_states()
+        self.dynamic_obs = self.get_dynamic_obstacles()
+        self.objects = {}
+
+        # self.curr_pos = torch.Tensor([0, 0], dtype=torch.int32)
+        self.curr_pos = np.array([0, 0], dtype=np.int32)
+
+        self.action_state_offset = {
+            -1: np.array([0,0]),
+            0: np.array([0,-1]),
+            1: np.array([1,0]),
+            2: np.array([0,1]),
+            3: np.array([-1,0])
+        }
+        self.curr_state = self.make_state()
+
+        self._n_episodes += 1
+        return self.curr_state, self.get_valid_actions()
 
     def get_valid_actions(self):
-        pass # can remove
+        self.valid_actions = np.array((np.random.rand(self.n_actions) <= self.action_prob), dtype=int)
+        # Make sure that there is at least one available action always.
+        while not self.valid_actions.any():
+            self.valid_actions = np.array((np.random.rand(self.n_actions) <= self.action_prob), dtype=int)
+
+        return self.valid_actions
 
     def step(self, action):
         assert self.valid_actions[action]
@@ -57,27 +132,54 @@ class Gridworld_687(object):
 
         reward += self.step_reward
 
+        actual_action = action
+        transition = True
+        # Add Randomness
+        rng = np.random.rand()
+        if rng < self.randomness:
+            if rng < self.randomness / 4:
+                actual_action = (action - 1) % self.n_actions
+            elif rng < self.randomness / 2:
+                actual_action = (action + 1) % self.n_actions
+            else:
+                actual_action = -1
 
+        new_pos = self.curr_pos + self.action_state_offset[actual_action]
 
-    # reward values associated with states
-    def set_rewards(self):
-        # All rewards
-        self.G1_reward = -10 #100
-        self.G2_reward = +10 #- 5
+        if self.valid_pos(new_pos):
+            reward += self.movement_reward
+            self.curr_pos = new_pos
+            reward += self.get_goal_rewards(self.curr_pos)
+        else:
+            reward += self.collision_reward
+            transition = False
+
+        if self.is_terminal():
+            transition = False
+        
+        if transition:
+            self.curr_state = self.make_state()
+
+        if self.debug:
+            # print(self.curr_pos)
+            pass
+
+        return self.curr_state.copy(), reward, self.get_valid_actions(), self.is_terminal(), {'No INFO implemented yet'}
 
     def make_state(self):
         x, y = self.curr_pos
         state = [x, y]
-
-        # TODO
 
         return state
 
     def get_goal_rewards(self, pos):
         for key, val in self.reward_states.items():
             region, reward = val
-            if self.debug: print("Got reward {} in {} steps!! ".format(reward, self.steps_taken))
-            return reward
+            if reward and self.in_region(pos, region):
+                self.reward_states[key]
+                if self.debug: print("Got reward {} in {} steps!! ".format(reward, self.steps_taken))
+                
+                return reward
         return 0
 
     # States where reward exist
@@ -95,12 +197,16 @@ class Gridworld_687(object):
         
         return obstacles
 
+    def get_dynamic_obstacles(self):
+        return []
+
     # Returns whether a position is valid
     def valid_pos(self, pos):
         flag = True
 
         # Check position within boundaries
         if not self.in_region(pos, [0,0,self.width - 1,self.width - 1]):
+            # print(pos, "not valid")
             flag = False
 
         # Checks if position is in obstacle
@@ -122,9 +228,31 @@ class Gridworld_687(object):
             return False
 
     def is_terminal(self):
-        if self.in_region(self.curr_pos, self.G1):
+        if self.in_region(self.curr_pos, self.G2):
             return True
         elif self.steps_taken >= self.max_steps:
             return True
         else:
             return False
+
+if __name__=="__main__":
+    # Random Agent
+    np.random.seed(0)
+    rewards_list = []
+    env = Gridworld_687(debug=True)
+    for i in range(50):
+        rewards = 0
+        done = False
+        _, valid = env.reset()
+        while not done:
+            # env.render()
+            # plt.show()
+            available = np.where(valid)[0]
+            action = np.random.choice(available)
+            next_state, r, valid, done, _ = env.step(action)
+            rewards += r
+
+        # print(env.steps_taken)
+        rewards_list.append(rewards)
+
+    print("Average random rewards: ", np.mean(rewards_list), np.sum(rewards_list))
--- other/Src/run.py
+++ other/Src/run.py
@@ -1,14 +1,25 @@
 #!~miniconda3/envs/rl/bin python
 import hydra
 from omegaconf import DictConfig, OmegaConf
+import Environments
 import Src.Algorithms.Agent as Agent
+from Environments.Gridworld_687 import Gridworld_687
 from time import time
+import torch
+import numpy as np
 
 class Solver:
     def __init__(self, config):
         
         self.config = config
-        self.env = self.config.env
+        self.env = self.Get_Environment(config)
+        self.state_dim = np.shape(self.env.reset())[0]
+
+    def Get_Environment(self, config):
+        env_name = config.env.name
+
+        if env_name == "Gridworld_687":
+            return Gridworld_687() #TODO: pass config params
         
 
 @hydra.main(config_name="config")
