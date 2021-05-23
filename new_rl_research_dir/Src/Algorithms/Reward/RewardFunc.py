33465de3e433f93ebc7e5b08281fcc5676e59218
mfclinton <mfclinton@umass.edu>
2021-05-23 00:05:03 -0400

    cur setup

--- new_rl_research_dir/Src/Algorithms/Reward/RewardFunc.py
+++ new_rl_research_dir/Src/Algorithms/Reward/RewardFunc.py
@@ -1,9 +1,20 @@
 import numpy as np
 import torch
+from Src.Algorithms.Algorithm import Algorithm
+import torch.nn as nn
 
-class RewardFunc():
-    def __init__(self):
-        self.initialized = False
+class RewardFunc(Algorithm):
+    def __init__(self, state_dim, config, optim=torch.optim.Adam, lr=.01):
+        super(RewardFunc, self).__init__()
 
-    def init(self, config):
-        self.initialized = True
+        self.fc1 = nn.Linear(self.state_dim, 1)
+        self.init_optimizer(optim, lr)
+
+
+    def forward(self, state):
+        x = self.fc1(state)
+        return x
+
+    
+
+        
