659edfd10acc23b41123092388f2986f5f83e80b
mfclinton <mfclinton@umass.edu>
2021-05-27 19:34:53 -0400

    changes

--- new_rl_research_dir/run_slurm_scan.sh
+++ new_rl_research_dir/run_slurm_scan.sh
@@ -6,10 +6,10 @@
 #SBATCH --partition=defq    # Partition to submit to
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
-#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=4
 #SBATCH --time=0-5:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=100    # Memory in MB per cpu allocated
+#SBATCH --mem-per-cpu=10    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mfclinton@umass.edu
 
