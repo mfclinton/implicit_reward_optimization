faca67d3f71ce9ff90605d0351552d9e3d4c3cf9
mfclinton <mfclinton@umass.edu>
2021-05-27 19:55:06 -0400

    changes

---
+++ new_rl_research_dir/run_slurm_0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH --job-name=run1
+#SBATCH --output=outputs/stdoutput/res_%j.txt  # output file
+#SBATCH -e outputs/stdoutput/res_%j.err        # File to which STDERR will be written
+#SBATCH --partition=defq    # Partition to submit to
+#
+#SBATCH --ntasks=30
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=0-5:59         # Maximum runtime in D-HH:MM
+#SBATCH --mem-per-cpu=200    # Memory in MB per cpu allocated
+#SBATCH --mail-type=END
+#SBATCH --mail-user=mfclinton@umass.edu
+#
+#SBATCH --array=0-30
+
+export MKL_NUM_THREADS=2
+export OPENBLAS_NUM_THREADS=2
+export OMP_NUM_THREADS=2
+
+
+python -m Src.run -cn config_GW seed=$SLURM_ARRAY_TASK_ID num_runs=1 config.name=regular_method config.env.aux_r_id=0 config.T1=40 config.T2=100 config.T3=25 config.batch_size=200
+
+sleep 1
