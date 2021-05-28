46eb621c348954f9603a70fe0052f464fea640c1
mfclinton <mfclinton@umass.edu>
2021-05-27 21:06:57 -0400

    modified run scripts

--- new_rl_research_dir/run_all.sh
+++
@@ -1,5 +0,0 @@
-for VARIABLE in {1..30}
-do
-    python -m Src.run -cn config_GW &
-    sleep 5
-done
--- new_rl_research_dir/run_slurm_-1.sh
+++ new_rl_research_dir/run_slurm_-1.sh
@@ -6,10 +6,10 @@
 #SBATCH --partition=defq    # Partition to submit to
 #
 #SBATCH --ntasks=30
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=12
 #SBATCH --time=0-5:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=200    # Memory in MB per cpu allocated
+#SBATCH --mem-per-cpu=100    # Memory in MB per cpu allocated
 #SBATCH --mail-type=END
 #SBATCH --mail-user=mfclinton@umass.edu
 #
@@ -20,6 +20,6 @@ export OPENBLAS_NUM_THREADS=2
 export OMP_NUM_THREADS=2
 
 
-python -m Src.run -cn config_GW seed=$SLURM_ARRAY_TASK_ID num_runs=1 config.name=regular_method config.env.aux_r_id=-1 config.T1=40 config.T2=100 config.T3=25 config.batch_size=200
+python -m Src.run -cn config_GW seed=$SLURM_ARRAY_TASK_ID num_runs=1 config.name=regular_method config.env.aux_r_id=-1
 
 sleep 1
