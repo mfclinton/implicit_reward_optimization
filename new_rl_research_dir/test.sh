4dc0c8fdf3162db3cb040186763c5380389f9fe0
mfclinton <mfclinton@umass.edu>
2021-05-29 20:18:49 -0400

    test

---
+++ new_rl_research_dir/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=run
+#SBATCH --output=outputs/stdoutput/res_%j.txt  # output file
+#SBATCH -e outputs/stdoutput/res_%j.err        # File to which STDERR will be written
+#SBATCH --partition=defq    # Partition to submit to
+#
+#SBATCH --ntasks=30
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=0-5:59         # Maximum runtime in D-HH:MM
+#SBATCH --mem-per-cpu=250    # Memory in MB per cpu allocated
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ateeter@umass.edu
+#
+#SBATCH --array=0-30
+
+export MKL_NUM_THREADS=2
+export OPENBLAS_NUM_THREADS=2
+export OMP_NUM_THREADS=2
+
+echo test
+
+sleep 1
