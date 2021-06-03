555efa87582eada8cd521f49cdc51fbb86f12855
mfclinton <mfclinton@umass.edu>
2021-06-03 01:49:54 -0400

    thing

--- new_rl_research_dir/run_slurm_scan.sh
+++ new_rl_research_dir/run_slurm_scan.sh
@@ -5,15 +5,13 @@
 #SBATCH -e outputs/stdoutput/res_%j.err        # File to which STDERR will be written
 #SBATCH --partition=defq    # Partition to submit to
 #
-#SBATCH --ntasks=1
+#SBATCH --ntasks=200
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=4
-#SBATCH --time=0-5:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=20000    # Memory in MB per cpu allocated
+#SBATCH --time=0-11:59         # Maximum runtime in D-HH:MM
+#SBATCH --mem-per-cpu=200    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mfclinton@umass.edu
-#
-#SBATCH --array=0-4
 
 export MKL_NUM_THREADS=2
 export OPENBLAS_NUM_THREADS=2
